7161c8fb1ec2d24808e400272ee8652b58c59710
Vadim Lavorchuk <lavorchukvadim@gmail.com>
2016-06-07 02:42:03 +0300

    start fixing author name in blog.index

--- resources/views/blog/userblogs.blade.php
+++ resources/views/blog/userblogs.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
     <div class="container">
+
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+                <a class="btn btn-warning" href="/blog"><i class="fa fa-btn fa-chevron-left"></i>Back</a>
+            </div>
+            <div class="col-md-10 col-md-offset-1">
             <h2 class="alert alert-success">{{ $user->name }} blogs</h2>
+
                 @foreach($user->blogs as $blog)
                     <div class="panel panel-default">
                         <div class="panel-heading clearfix">
